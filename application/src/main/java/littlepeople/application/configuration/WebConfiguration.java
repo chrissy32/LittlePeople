a4c878e27591a5d0300aab54c4c593e3ab00bd26
Cristiana <ircristiana32@gmail.com>
2019-11-19 16:22:07 +0200

    Project Setup: security config

--- application/src/main/java/littlepeople/application/configuration/WebConfiguration.java
+++ application/src/main/java/littlepeople/application/configuration/WebConfiguration.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,5 +30,12 @@ public class WebConfiguration extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 
         return source;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and().csrf().disable();
     }
 }
